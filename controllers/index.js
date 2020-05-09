2f8671ae0eefa569dd1d55fc72c8a3ca6d479dae
dhruv sharma <dhruvshubham@gmail.com>
2020-05-09 11:44:32 -0700

    kraken remove

--- controllers/index.js
+++ controllers/index.js
@@ -1,28 +1,10 @@
 'use strict';
+var express = require('express');
+var router = express.Router();
 
-//var businessLayer = require('./common/logic');
+router.get('/', function (req, res) {        
+    res.render( 'common/index',{
+    });        
+});
 
-module.exports = function (router) {
-    router.get('/', function (req, res) {        
-        res.render( 'common/index',{
-        });        
-    });
-    //router.post('/users/authenticate/',function(req,res){        
-    //        businessLayer.createNewUser(req.body,function(err, user){
-    //            if (err) {
-    //                res.statusCode = 400;
-    //                res.send({
-    //                    'message':err,
-    //                    'name':'Error'
-    //                });
-    //            }
-    //            else{
-    //                res.send({
-    //                    'details':user,
-    //                    'name':'OK'
-    //                });
-    //            }
-    //        });
-    //   
-    //});
-};
+module.exports = router;
--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-'use strict';
-
 var express = require('express');
-var kraken = require('kraken-js');
-var options, app;
-app = module.exports = express(),
-options = require('./lib/spec')(app);
-app.use(kraken(options));
-app.on('start', function () {
-    console.log('Application ready to serve requests.');
-    console.log('Environment: %s', app.kraken.get('env:env'));
+var createError = require('http-errors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./controllers/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/public', express.static('public'));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
